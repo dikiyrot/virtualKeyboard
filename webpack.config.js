05e9ff89bdf5cbc1268efcac693d120cd9bbd3c9
dikiyrot <dikiyrot@gmail.com>
2022-05-10 02:48:30 +0300

    Feat: add eslint

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[fullhash].js'
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
